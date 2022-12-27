8c97bc130e34838bde8c9986b574377110e1c872
layne nigro <laynenigro@gmail.com>
2022-12-26 19:38:50 -0500

    added day-view.js and day.handlebars with CSS

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,10 +3,13 @@ const { User, Workout } = require('../models');
 // const withAuth = require('../utils/auth');
 
 // router.get('/'), render login page
+router.get('/', async (req, res) => {
+    res.json('Hello World');
+});
 
 // router.get('/calendar'), render calendar
 
 // router.get('/day/id'), render day (id = date)
 
 
-
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
