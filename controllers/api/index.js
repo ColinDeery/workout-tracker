1e4e607a04d2be8c600936fedda3c8e7133218a1
layne nigro <laynenigro@gmail.com>
2023-01-04 13:16:54 -0500

    update prior to merge

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const workoutRoutes = require('./workoutRoutes');
-const previousWorkoutRoutes = require('./previousWorkoutRoutes');
 
 router.use('/users', userRoutes);
 router.use('/workout', workoutRoutes);
-router.use('/:date', previousWorkoutRoutes);
 
 module.exports = router;
