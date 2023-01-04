349e65463b745078aa03873fde64cb8b0aed445f
avanim <axm1205@case.edu>
2023-01-03 22:42:31 -0500

    added loginFormHandler to bring user to /calendar once logged in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,44 +1,44 @@
-// const router = require('express').Router();
-// const { User } = require('../../models');
+const router = require('express').Router();
+const { User } = require('../../models');
 
-// // Login
-// router.post('/login', async (req, res) => {
-//     try {
-//         // Find User with same username provided
-//         const dbUserData = await User.findOne({
-//             where: {
-//                 username: req.body.username
-//             }
-//         });
+// Login existing user
+router.post('/login', async (req, res) => {
+    console.log('\n Reached /api/users/login');
+    try {
+        // Find User with same username provided
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
 
-//         // If no user found with provided username
-//         if (!dbUserData) {
-//             res.status(404).json({ message: 'Incorrect username or password. Please try again!' });
-//             return;
-//         }
+        // If no user found with provided username
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
 
-//         // If user found with username, check provided password against user's encrypted password
-//         const validPassword = await dbUserData.checkPassword(req.body.password);
+        // If user found with username, check provided password against user's encrypted password
+        const validPassword = await dbUserData.checkPassword(req.body.password);
 
-//         // If password is not the same as saved password in db
-//         if (!validPassword) {
-//             res.status(404).json({ message: 'Incorrect username or password. Please try again!' });
-//             return;
-//         }
+        // If password is not the same as saved password in db
+        if (!validPassword) {
+            res.status(404).json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
 
-//         // If password correct, set loggedIn to true
-//         req.session.save(() => {
-//             req.session.loggedIn = true;
-//             req.session.userId = dbUserData.id;
-//             console.log('\nLogged in\n');
-//             res.status(200).json({ user: dbUserData, message: 'Successfully logged in!' });
-//             return;
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
+        // If password correct, set loggedIn to true
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.userId = dbUserData.id;
+            res.status(200).json({ user: dbUserData, message: 'Successfully logged in!' });
+            return;
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 // // Sign up
 // router.post('/signup', async (req, res) => {
@@ -74,4 +74,4 @@
 //     }
 // });
 
-// module.exports = router;
+module.exports = router;
